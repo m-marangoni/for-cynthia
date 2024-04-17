114b724d258e2244e8aae806be103b430a91d201
Mariana <marangoni.mariana@gmail.com>
2024-04-17 01:02:53 +0100

    Update listening.sh

--- listening.sh
+++ listening.sh
@@ -10,11 +10,11 @@ open_index_html() {
 
 # Loop to continuously listen to USB events
 while true; do
-    # Wait for a USB device to be connected
-    udevadm monitor --subsystem-match=usb --action=add | \
+    # Monitor USB events
+    udevadm monitor --environment --subsystem-match=usb | \
     while read -r line; do
-        # Check if the line contains information about the USB device
-        if [[ $line == *"usb"* ]]; then
+        # Check if the line contains information about the USB device being added
+        if [[ $line == *"ACTION=add"* ]]; then
             # Open index.html when a USB device is connected
             open_index_html
         fi
